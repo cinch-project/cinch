3247ad20cf7407b74d21ba376e67981c4b5f1767
Andrew Chernow <achernow@gmail.com>
2022-12-17 20:07:28 -0500

    project path assertion to ensure it does not contain forward slashes.

--- src/Project/ProjectName.php
+++ src/Project/ProjectName.php
@@ -9,6 +9,7 @@ class ProjectName extends SingleValue
 {
     public function __construct(string $name)
     {
+        Assert::notContains($name, '/', message: 'project name');
         Assert::regex($name, "~^[\x{0001}-\x{ffff}]{1,57}(?<!\s)$~u", 'project name');
         parent::__construct($name);
     }
