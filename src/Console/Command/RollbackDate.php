10b794c28109508c57591c0b1ef067fcae4ead05
Andrew Chernow <achernow@gmail.com>
2023-01-03 22:35:48 -0500

    added rollback date and script

---
+++ src/Console/Command/RollbackDate.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Cinch\Console\Command;
+
+use Cinch\Command\RollbackBy;
+use Cinch\Console\Command;
+use Exception;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand('rollback:date', 'Roll back to a specific date')]
+class RollbackDate extends Command
+{
+    /**
+     * @throws Exception
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $date = $this->parseDateValue($input->getArgument('date'));
+        $this->executeRollback($input, RollbackBy::date($date), 'rolling back to ' . $date->format('Y-m-d\TH:i:sP'));
+        return self::SUCCESS;
+    }
+
+    protected function configure()
+    {
+        $tz = system_time_zone();
+        $this
+            ->addProjectArgument()
+            ->addArgument('date', InputArgument::REQUIRED, "Roll back to this date")
+            ->addOptionByName('deployer')
+            ->addOptionByName('tag')
+            ->addOptionByName('dry-run')
+            ->addOptionByName('env')
+            ->setHelp(<<<HELP
+All changes since (greater than) <info><date></info>, will be rolled back. <info><date></info> can be in the 
+below formats:
+
+     date 2022-09-28 - default time is 00:00:00     
+     time 09:32:10 - default date is today (seconds can be omitted 09:32 -> 09:32:00)
+ datetime 2022-09-28T09:32:10 - the 'T' separator is required
+time zone 09:32:10-0400, 09:32:10+04:00, etc. - defaults to system (currently '$tz')
+
+<code-comment># roll back all changes since 7am today system time</>
+<code>cinch rollback:date project 07:00 --tag=clear-the-day</>
+HELP
+            );
+    }
+}
---
+++ src/Console/Command/RollbackScript.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Cinch\Console\Command;
+
+use Cinch\Command\RollbackBy;
+use Cinch\Common\StorePath;
+use Cinch\Console\Command;
+use Exception;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand('rollback:script', 'Roll back one or more migration scripts')]
+class RollbackScript extends Command
+{
+    /**
+     * @throws Exception
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $paths = [];
+        $rawPaths = [];
+
+        foreach ($input->getArgument('paths') as $p) {
+            $paths[] = new StorePath($p);
+            $rawPaths[] = $p;
+        }
+
+        $title = "Rolling back migration scripts " . implode(', ', $rawPaths);
+        $this->executeRollback($input, RollbackBy::script($paths), $title);
+        return self::SUCCESS;
+    }
+
+    protected function configure()
+    {
+        $this
+            ->addProjectArgument()
+            ->addArgument('paths', InputArgument::REQUIRED | InputArgument::IS_ARRAY, 'One or more migration store paths')
+            ->addOptionByName('deployer')
+            ->addOptionByName('tag')
+            ->addOptionByName('dry-run')
+            ->addOptionByName('env')
+            ->setHelp(<<<HELP
+The given <info><paths></info> must be migrations scripts that have already been deployed.
+
+<code-comment># roll back previously deployed migration scripts</>
+<code>cinch rollback:script project 2022/09/a.sql 2022/10/b.sql</>
+HELP
+            );
+    }
+}
