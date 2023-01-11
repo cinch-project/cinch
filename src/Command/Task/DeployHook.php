9e4ca7d77224b1d4e19b5193221d557cbaa4019a
Andrew Chernow <achernow@gmail.com>
2023-01-11 05:53:01 -0500

    added task for deploy hooks

---
+++ src/Command/Task/DeployHook.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Cinch\Command\Task;
+
+use Cinch\Command\HookRunner;
+use Cinch\Command\Task;
+use Cinch\Hook;
+
+class DeployHook extends Task
+{
+    public function __construct(
+        private readonly Hook\Hook $hook,
+        private readonly Hook\Event $event,
+        private readonly Deploy $deployTask,
+        private readonly HookRunner $hookRunner)
+    {
+        $action = $this->hook->action;
+        parent::__construct(
+            "hook: $event->value",
+            sprintf('%s: %s', $action->getType()->value, $action->getPath())
+        );
+    }
+
+    protected function doRun(): void
+    {
+        $this->hookRunner->runHook($this->hook, $this->event, $this->deployTask->getChange());
+    }
+
+    protected function doUndo(): void
+    {
+    }
+}
